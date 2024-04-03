665b49f4c5b0f9ac88dcc0646c072632ccc7d1cd
WIKse96 <105929893+WIKse96@users.noreply.github.com>
2024-04-03 15:59:29 +0200

    add to cart locators

--- pages/locators/prod_card_page_locators.py
+++ pages/locators/prod_card_page_locators.py
@@ -1,3 +1,10 @@
 class ProdCardPage_locators:
+    # locators for grouped products
     SKU_GROUPED = "//div[@class='value']"
     SKU_SIMPLE_marked = "//div[@class='sku sku-grupped']/mark"
+    # locators for simple products
+    PRICE_SIMPLE = "//span[@class='price']"
+    ADDTOCART_BTN = "//button[@type='submit'][@id='product-addtocart-button']"
+    QTY_INPUT = "//input[@id='qty'][@type='number']"
+    QTY_UP_ARROW = "//i[@class='porto-icon-up-dir']"
+    QTY_DOWN_ARROW = "//i[@class='porto-icon-down-dir']"
--- pages/page_obj/prod_page.py
+++ pages/page_obj/prod_page.py
@@ -6,6 +6,7 @@ from pages.page_obj.base_page import BasePage
 
 class ProdPage(BasePage):
     path = None
+    path_prod_simple = 'catalog/product/view/id/5224/s/bateria-kuchenna-stojaca-czarna-flower/category/56/'
 
     def __init__(self, page: Page):
         super().__init__(page)
@@ -15,3 +16,6 @@ class ProdPage(BasePage):
     def assert_search(self, search_query: str):
         expect(self.sku_grouped).to_be_visible()
         expect(self.sku_marked).to_have_text(search_query)
+
+    def add_to_cart_by_arrow(self, qty: str):
+        pass
