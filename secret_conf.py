762085bfb595a7f5e06be7c72833031d508b4a76
WIKse96 <105929893+WIKse96@users.noreply.github.com>
2024-06-11 11:09:05 +0200

    Create secret_conf.py

---
+++ secret_conf.py
@@ -0,0 +1,5 @@
+import os
+
+PASSWORD = os.environ.get('PASSWORD', 'default_password')
+USERNAME = os.environ.get('USERNAME', 'default_username')
+EMAIL_FOR_REMIND = os.environ.get('EMAIL_FOR_REMIND', 'default_email')
