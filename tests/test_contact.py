f4e60e4a235f630c0cbcc8d59fa0837f4fd78d42
WIKse96 <105929893+WIKse96@users.noreply.github.com>
2024-08-21 08:14:41 +0200

    contact - reorg

--- tests/test_contact.py
+++ tests/test_contact.py
@@ -1,5 +1,60 @@
+import time
+
+from playwright.sync_api import expect
+from support.support import check_text
 from pages.page_obj.contact_page import ContactPage
+
 import pytest
+
+
+
+def perform_contact_form(contact_page: ContactPage,
+                         company_name: str,
+                         subject: str,
+                         email: str,
+                         message: str,
+                         rodo: bool,
+                         phone_nr: str,
+                         ):
+    contact_page.load(contact_page.path, wait='domcontentloaded')
+    time.sleep(2)
+
+    # Wypełnij formularz danymi z parametrów
+    if company_name:
+        contact_page.compan_name_only(company_name)
+    if subject:
+        contact_page.subject_only(subject)
+    if email:
+        contact_page.email_only(email)
+    if phone_nr:
+        contact_page.phone_only(phone_nr)
+    if message:
+        contact_page.message_only(message)
+    contact_page.rodo_only(rodo)
+
+    # Kliknij przycisk Submit
+    # time.sleep(3)
+    # contact_page.pause()
+    contact_page.submit_click()
+
+    # Sprawdź, czy oczekiwane błędy pojawiły się
+    errors_dic = contact_page.get_errors()
+
+    if company_name == '':
+        expect(errors_dic['name_error']).to_be_visible()
+    if subject == '':
+        expect(errors_dic['subject_error']).to_be_visible()
+    if message == '':
+        expect(errors_dic['comment_error']).to_be_visible()
+
+    if not rodo:
+        expect(errors_dic['rodo_error']).to_be_visible()
+    if email == '':
+        expect(errors_dic['email_error']).to_be_visible()
+    else:
+        check_text(email)
+        expect(errors_dic['email_error']).to_contain_text("Podaj poprawny adres email (np.: johndoe@domain.com).")
+
 def test_map_vsibility(contact_page: ContactPage):
     contact_page.load(contact_page.path, wait='domcontentloaded')
     contact_page.map_checker()
@@ -7,10 +62,28 @@ def test_map_vsibility(contact_page: ContactPage):
 
 
 @pytest.mark.run
-def test_contact_blank_form(contact_page: ContactPage):
-    contact_page.load(contact_page.path, wait='domcontentloaded')
-    contact_page.blank_form()
-@pytest.mark.run
-def test_contact_rodo_only(contact_page: ContactPage):
-    contact_page.load(contact_page.path, wait='domcontentloaded')
-    contact_page.only_rodo()
+@pytest.mark.parametrize("company_name, subject, email, message, rodo, phone_nr",
+                         [
+                         # ('', '', '', '', False, ''),  # Wszystko puste
+                         #  ('', '', '', '', True, ''),  # Wszystko puste, checkbox zaznaczony
+                         #  ('NAZWA FIRMY ĄŚĆŻ', '', '', '', False, ''),  # Tylko company_name
+                         #  ('', '', '', '', False, '55566644-'),  # Tylko telefon
+                         #  ('', '', 'test@seart.pl', '', False, ''),  # Tylko email
+                          # ('', '', '', 'Wiadomość żźćół 86 /*-', False, ''),  # Tylko wiadomość
+                          # ('', '', '', '', True),  # Przypadek pozytywny
+                          ('', '', 'test.seart.pl', '', False, ''),  # Niepoprawny email (bez @)
+                          ('', '', 'test@pl', '', False, ''), # Niepoprawny email (bez .com)
+                          ])
+def test_contact_form(contact_page: ContactPage, company_name: str, subject: str,
+                      email: str, message: str, rodo: bool, phone_nr: str):
+
+    perform_contact_form(
+        contact_page=contact_page,
+        company_name=company_name,
+        subject=subject,
+        email=email,
+        message=message,
+        rodo=rodo,
+        phone_nr=phone_nr,
+
+    )
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,20 +1,59 @@
 import pytest
 from secret_conf import PASSWORD, USERNAME
 from pages.page_obj.login_page import LoginPage
+from playwright.sync_api import expect
 
 
-@pytest.mark.login
-def test_login(login_page: LoginPage):
+# Funkcja pomocnicza do logowania i weryfikacji poprawności lub błędów
+def perform_login_and_check(
+        login_page: LoginPage,
+        username: str,
+        password: str,
+        expected_email_error: str = None,
+        expected_pass_error: str = None,
+        expect_correct_login: bool = False
+):
     login_page.load(path=login_page.path)
-    login_page.login(USERNAME, PASSWORD)
-    login_page.save_context()
+    login_page.login(username, password)
 
-# @pytest.mark.run
-def test_login_incorrect_email(login_page: LoginPage):
-    pass
+    if expect_correct_login:
+        login_page.correct_login_info()
+        login_page.save_context()
+    else:
+        if expected_email_error or expected_pass_error:
+            errors_dict = login_page.get_errors()
+            pass_error = errors_dict.get('pass_error')
+            email_error = errors_dict.get('email_error')
 
-# @pytest.mark.run
-def test_login_incorrect_password(login_page: LoginPage):
-    pass
+            if expected_email_error is not None:
+                expect(email_error).to_be_visible() if expected_email_error else expect(email_error).to_be_hidden()
+
+            if expected_pass_error is not None:
+                expect(pass_error).to_be_visible() if expected_pass_error else expect(pass_error).to_be_hidden()
+        else:
+            login_page.incorrect_login_info()
 
 
+# Parametryzowane testy dla różnych przypadków logowania
+@pytest.mark.parametrize("username, password, expected_email_error, expected_pass_error, expect_correct_login", [
+    (USERNAME, PASSWORD, None, None, True),  # Poprawne dane logowania
+    ('USERNAME', PASSWORD, True, None, False),  # Niepoprawny email
+    (USERNAME, 'PASSWORD', None, True, False),  # Niepoprawne hasło
+    ('USERNAME@op.pl', 'PASSWORD', True, True, False),  # Niepoprawny email i hasło
+    ('', '', True, True, False),  # Puste pola email i hasło
+    ('', 'jakieshalo123', True, None, False),  # Pusty email
+    (USERNAME, '', None, True, False),  # Puste hasło
+    ("USERNAME.com", '456987pdp', True, None, False),  # Brak '@' w emailu
+    ("USERNAME@com", '456987pdp', True, None, False)  # Brak '.' w emailu
+])
+@pytest.mark.login
+def test_login(login_page: LoginPage, username: str, password: str, expected_email_error: str, expected_pass_error: str,
+               expect_correct_login: bool):
+    perform_login_and_check(
+        login_page=login_page,
+        username=username,
+        password=password,
+        expected_email_error=expected_email_error,
+        expected_pass_error=expected_pass_error,
+        expect_correct_login=expect_correct_login
+    )
